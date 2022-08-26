f98b716feab6422766c55f9ff5f3aa532b5403db
PRIYANSHU_BURMAN <priyanshu887887@gmail.com>
2022-08-26 19:43:25 +0530

    365DaysJava

---
+++ day7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class day7 { 
+ public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = in.nextInt();
+        System.out.print("Check  every digit of the number is even or not!");
+        System.out.print(test(n));
+        }
+
+  public static boolean test(int n){
+     int x = 10;
+    if (n == 0){
+        return false;
+    }
+    while(n != 0){
+        if((n % x) % 2 != 0){
+            return false;
+        }
+        n /= 10;
+    }
+     return true;
+   }
+}
